937518cd526a846d1c625fd491b5935cd0b9ab50
lsy <872136241@qq.com>
2023-09-20 10:15:55 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <div id="app">
+      <StudentResume />
+    </div>
+  </template>
+  
+  <script>
+  import StudentResume from './components/StudentResume.vue';
+  
+  export default {
+    components: {
+      StudentResume,
+    },
+  };
+  </script>
---
+++ src/components/StudentResume.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="student-resume">
+    <div class="header">
+      <img class="avatar" src="@/assets/avatar.jpg" alt="Avatar" />
+      <h1>{{ studentName }}</h1>
+      <p>{{ studentNickname }}</p>
+      <p>{{ studentID }}</p>
+    </div>
+    <h2>教育背景</h2>
+    <ul>
+      <li>{{ education }}</li>
+    </ul>
+    <h2>工作经验</h2>
+    <ul>
+      <li>{{ experience }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      studentName: 'Lsy',
+      studentNickname: 'Bonder007',
+      studentID: '42354324',
+      education: '辽宁工程技术大学- University of Example, 2023',
+      experience: '华为实习 (2020 - Present)',
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* 样式可以根据需要自行添加 */
+.header {
+  text-align: center;
+}
+
+.avatar {
+  width: 150px; /* 调整头像尺寸 */
+  height: 150px;
+  border-radius: 50%; /* 圆形头像 */
+  margin: 10px auto;
+  display: block;
+}
+</style>
